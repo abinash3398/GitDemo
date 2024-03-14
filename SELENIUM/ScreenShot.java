01eee85d6c319ce1e0e3014dfb295c2dafb326ab
Abinash Dash <109858192+abinash3398@users.noreply.github.com>
2024-03-14 15:53:07 +0530

    Update ScreenShot.java

--- SELENIUM/ScreenShot.java
+++ SELENIUM/ScreenShot.java
@@ -1,17 +1,23 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.google.common.io.Files;
+
 import java.io.File;
+import java.io.IOException;
+
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+
 public class ScreenShot {
 	static WebDriver driver;
-	public static void main(String[] args) {
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.amazon.in/");
-			}
-public static void takeScreenshot(String filename)
-{
- File file=  ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
 
-}
+	public static void main(String[] args) throws IOException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/locatorspractice/");
+
+		File file = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		Files.copy(file, new File("E:\\rep\\rscreenshot.jpg"));
+	}
 }
