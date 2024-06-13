1041bdf98ce28d06907c0461af3710f4218fffbe
Abinash Dash <109858192+abinash3398@users.noreply.github.com>
2024-06-13 20:04:14 +0530

    Add files via upload

---
+++ python_projects/itunes.py
@@ -0,0 +1,10 @@
+import requests
+import sys
+
+if len(sys.argv)!=2:
+    exit
+response=requests.get("https://itunes.apple.com/search?entity=song&limit=1&term="+ sys.argv[1])
+o=response.json()
+for result in o['results']:
+    print(result['trackName'])    
+
